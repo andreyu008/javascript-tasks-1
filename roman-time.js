31b30b60047968ce7b057221f07126c07eaefd04
andreyu008 <Andreyu008@gmail.com>
2015-10-29 14:26:33 +0500

    Доработанный вариант @grumpy

--- roman-time.js
+++ roman-time.js
@@ -1,27 +1,25 @@
 var hours = parseInt(process.argv[2], 10);
 var minutes = parseInt(process.argv[3],10);
 
-if (hours >= 0 && hours <=23 && minutes >=0 && minutes<=60) {
+function toRoman(number){
+    var tens = Math.floor(number / 10) * 10;
+    var units = number - tens;
+    var hours = romans[tens] + romans[units];
+    return hours;
+}
+
+if (hours >= 0 && hours <=23 && minutes >=0 && minutes<=59) {
 
     var romans = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
     romans [20] = 'XX';
     romans [30] = 'XXX';
     romans [40] = 'XL';
     romans [50] = 'L';
-    var hoursTens = Math.floor(hours / 10) * 10;
-    var hoursUnits = hours % 10;
-    var romanHours = romans[hoursTens] + romans[hoursUnits];
-    var minutesTens = Math.floor(minutes/10)*10;
-    var minutesUnits = minutes % 10;
-    var romanMinutes = romans[minutesTens]+romans[minutesUnits];
+
+    var romanHours = toRoman(hours);
+    var romanMinutes=toRoman(minutes);
+
     console.log(hours === 0 ? '-' : romanHours, romanMinutes);
 } else{
     console.log('Время указанно неверно');
 }
-
-
-
-
-
-
-// Немного замечательного кода и магии
